db9bddd1f2424af67eda30178570421f0fe1530c
yatin sharma <yatins113@gmail.com>
2026-01-19 11:46:15 +0530

    client side error

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -1,6 +1,5 @@
-export const dynamic = "force-dynamic"
-
 "use client"
+export const dynamic = "force-dynamic"
 
 import {
   AlertDialog,
--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 export const dynamic = "force-dynamic"
 
-"use client"
+
 
 import { useEffect, useState } from "react"
 import { useSearchParams, useRouter } from "next/navigation"
